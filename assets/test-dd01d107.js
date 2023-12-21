8e0c941e40bbe993f73280eb1aaad6872a94b678
yemaster <yemaster@oigat.cn>
2023-12-21 15:31:08 +0800

    Add

---
+++ assets/test-dd01d107.js
@@ -0,0 +1,1 @@
+const e={title:"不同题型测试",test:3,problems:[{content:"1+1=()",choices:["0","1","2","3","以上都不是"],answer:2,type:1},{content:"1+1<()",choices:["0","1","2","3","4","5","6"],answer:[0,1],type:2},{content:"1+1=(),1+2=()",answer:"2,3",type:3}]};export{e as problemInfo};
