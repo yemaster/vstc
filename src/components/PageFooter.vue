8fdaba18c9022ef01028d6c9a2d24977807a07de
yemaster <yemaster@oigat.cn>
2024-09-01 11:10:41 +0800

    Update Footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -11,11 +11,18 @@ function feedback() {
 <template>
     <div class="v-container" :class="{ 'hidden': $route.name === 'Test' }">
         <div class="v-divider"></div>
-        <div class="v-footer"> <router-link :to="{ name: 'Update Logs' }" #="{ navigate, href }" custom>
+        <div class="v-footer">
+            <n-a href="https://github.com/yemaster/vstc" target="_blank">Github</n-a>
+            |
+            <router-link :to="{ name: 'Update Logs' }" #="{ navigate, href }" custom>
                 <n-a :href="href" @click="navigate">
                     更新日志
                 </n-a>
-            </router-link> | <n-a href="javascript:;" @click="feedback">意见反馈</n-a> | Made by <n-a
-                href="https://github.com/yemaster" target="_blank">yemaster</n-a>.</div>
+            </router-link>
+            |
+            <n-a href="javascript:;" @click="feedback">意见反馈</n-a>
+            |
+            Made by <n-a href="https://github.com/yemaster" target="_blank">yemaster</n-a>.
+        </div>
     </div>
 </template>
