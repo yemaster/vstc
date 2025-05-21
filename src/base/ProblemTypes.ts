215d561c29044c63175c0d03945f9f5a235726e0
Yemaster <30290923+yemaster@users.noreply.github.com>
2025-05-21 16:13:00 +0800

    Update ProblemTypes.ts

--- src/base/ProblemTypes.ts
+++ src/base/ProblemTypes.ts
@@ -3,23 +3,27 @@ export type SingleChooseProblemType = {
     content: string;
     choices: string[];
     answer: number;
+    hint?: string;
 };
 export type MultiChooseProblemType = {
     type: 2;
     content: string;
     choices: string[];
     answer: number[];
+    hint?: string;
 };
 export type FillingProblemType = {
     type: 3;
     content: string;
     answer: string;
+    hint?: string;
 };
 export type JudgementProblemType = {
     type: 4;
     content: string;
     choices: [string, string];
     answer: number;
+    hint?: string;
 };
 export type ChooseProblemType = SingleChooseProblemType | MultiChooseProblemType | JudgementProblemType;
 export type ProblemType = ChooseProblemType | FillingProblemType;
